5459c9aa40fedd818d35f3f294bd0b549dd094c6
Jhon Luke Cutay <jhonlukecutayyy@gmail.com>
2023-02-03 17:28:34 +0800

    Add files via upload
    Converting hours to minutes.

---
+++ hoursToMinutes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int toMinutes(int hours) {
+    int toMinutes = hours * 60;
+}
+
+
+int main() {
+    int input;
+    cin >> input;
+    cout << toMinutes(input);
+
+    return 0;
+}
